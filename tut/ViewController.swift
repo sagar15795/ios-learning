fcff6c9f23bdd12321d527a3eb833503ef931094
Sagar <kumarsagar15795@gmail.com>
2016-08-06 23:01:10 +0530

    add button click event

--- tut/ViewController.swift
+++ tut/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var Text: UILabel!
+
+    @IBAction func ButtonPress(sender: UIButton) {
+        let title = sender.titleForState(.Normal)!
+        
+        Text.text="you clicked on \(title) button "
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
